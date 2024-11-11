631bbc3f2c9047784e0733e4a1de0d937c2cf3ad
Jesusdox1 <estudiantejesussistemas@gmail.com>
2024-11-11 14:23:36 -0500

    Update ScanData.js

--- models/ScanData.js
+++ models/ScanData.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose');
 const scanDataSchema = new mongoose.Schema({
   ip: { type: String, required: true },
   puertos_abiertos: { type: [String], required: true },  // Cambiado a String para almacenar IPs
+  exploits_realizados: [
+    {
+      puerto: { type: Number },
+      resultado: { type: String }
+    }
+  ],
   fecha: { type: Date, default: Date.now }
 });
 
